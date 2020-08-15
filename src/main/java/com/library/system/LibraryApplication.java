0de20cad070b0bff5b95fc769c9ca14e06ddefa2
biswassupriya <supriya0483@gmail.com>
2020-08-15 23:34:14 +0100

    Initial Commit

--- src/main/java/com/library/system/LibraryApplication.java
+++ src/main/java/com/library/system/LibraryApplication.java
@@ -12,14 +12,15 @@ public class LibraryApplication {
     public static void main(String[] args) throws InterruptedException {
         Library library = new Library();
         List<Book> books = new ArrayList<>();
-        for (int i = 1; i <= 20; i++) {
+        for (int i = 1; i <= 10; i++) {
             books.add(new Book(i));
         }
 
+        System.out.println("Adding " + books.size() + " Books to library");
         BookInventorySynchronisedService bookInventoryService = new BookInventorySynchronisedService(10);
         List<Book> insertedBooks = bookInventoryService.addBooks(books);
         library.setBooks(insertedBooks);
-        library.getBooks().forEach(book -> System.out.println("Books in Library " + book.getIsbn()));
+        library.getBooks().forEach(book -> System.out.println("Book ISBN: " + book.getIsbn()));
+        System.out.println("Added " + insertedBooks.size() + " Books to library");
     }
-
 }
